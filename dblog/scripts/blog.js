d5fb75fd81d56a97af4bbfb4059e83d2b65a545c
Demon000 <demonsingur@gmail.com>
2016-01-15 12:33:41 +0200

    Added states and links to articles

--- dblog/scripts/blog.js
+++ dblog/scripts/blog.js
@@ -1,10 +1,16 @@
 request = window.superagent;
+marked.setOptions({
+ highlight: function (code) {
+   return hljs.highlightAuto(code).value;
+ }
+});
 var dblog = new Vue(
  {
    el: 'body',
    data:
    {
      collection: [],
+     authors: {},
      limit: 4,
      page: 4,
      viewing: false,
@@ -29,6 +35,7 @@ var dblog = new Vue(
        .end(function(err, res)
        {
          self.article.content = marked(res.text);
+         history.pushState(null, null, '#article=' + article.id);
          self.viewing = true;
        });
 
@@ -36,6 +43,15 @@ var dblog = new Vue(
      ripple: ripple,
    }
  });
+ request
+ .get('authors/authors.json')
+ .end(function(err, res)
+ {
+   var resource = JSON.parse(res.text);
+   for(var author in resource)
+    dblog.authors[author] = resource[author];
+ });
+
  request
  .get('data/articles.json')
  .end(function(err, res)
@@ -45,4 +61,18 @@ var dblog = new Vue(
    {
      dblog.collection.push(article);
    });
+   if(gethash('article'))
+   {
+     var article = _.findWhere(dblog.collection, {id: gethash('article')});
+     console.log(article);
+     if(article)
+      dblog.view(article);
+   }
  });
+
+
+function gethash(key)
+{
+  var matches = location.hash.match(new RegExp(key+'=([^&]*)'));
+  return matches ? matches[1] : null;
+}
