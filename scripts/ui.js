7f82e39e6bb338b75c13cf457ead384974b7a872
Demon000 <demonsingur@gmail.com>
2016-02-02 11:17:06 +0200

    Stuff. Lots of stuff.

--- scripts/ui.js
+++ scripts/ui.js
@@ -2,7 +2,7 @@ var nav = document.querySelector('.nav');
 var contact = document.querySelector('.contact');
 document.onscroll = function(e)
 {
-  if(document.documentElement.scrollTop > 100)
+  if(document.documentElement.scrollTop > window.innerHeight / 3)
   {
     document.body.classList.add('scrolled')
   }
