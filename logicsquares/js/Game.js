ef8c6b45044ef800610331955cc359540503de1f
Demon <demonsingur@gmail.com>
2017-10-17 10:45:38 +0000

    logicsquares: add initial table implementation

--- logicsquares/js/Game.js
+++ logicsquares/js/Game.js
@@ -1,2 +1,27 @@
 (function() {
+    function GameTable(options) {
+        var gt = this;
+
+        var size = options.size;
+
+        var table = createElement('table', {
+            class: 'game-table'
+        });
+
+        for (var i = 0; i < size; i++) {
+            var row = createElement('tr', {
+               class: 'game-row',
+               parent: table
+            });
+
+            for (var j = 0; j < size; j++) {
+                var cell = createElement('td', {
+                    class: 'game-cell',
+                    parent: row
+                });
+            }
+        }
+        gt.element = table;
+    }
+    window.GameTable = GameTable;
 })();
