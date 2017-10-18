d5299f774e50aec03ec51052485f69ee11aa63d2
Demon <demonsingur@gmail.com>
2017-10-18 09:12:30 +0000

    logicsquares: move cell creation to a separate class

--- logicsquares/js/Game.js
+++ logicsquares/js/Game.js
@@ -1,4 +1,38 @@
 (function() {
+    function GameCell() {
+        var gc = this;
+
+        var value = Utils.randomBinary();
+
+        gc.element = createElement('td', {
+            class: 'game-cell'
+        });
+
+        gc.setValue = function(newValue) {
+            gc.element.setAttribute('data-value', newValue);
+            value = newValue;
+            return gc;
+        };
+
+        gc.flipValue = function() {
+            gc.setValue(!value);
+            return gc;
+        };
+
+        gc.initValue = function() {
+            gc.setValue(Utils.randomBinary());
+            return gc;
+        };
+
+        gc.getValue = function() {
+            return value;
+        };
+
+        gc.attach = function(parent) {
+            parent.appendChild(gc.element);
+        };
+    }
+
     function GameTable(options) {
         var gt = this;
 
@@ -15,13 +49,8 @@
             });
 
             for (var j = 0; j < size; j++) {
-                var cell = createElement('td', {
-                    class: 'game-cell',
-                    parent: row
-                });
-
-                var value = Utils.randomBinary();
-                cell.setAttribute('data-value', value);
+                var cell = new GameCell();
+                cell.initValue().attach(row);
             }
         }
         gt.element = table;
