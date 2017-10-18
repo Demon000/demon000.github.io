7e36e9ba7e8e5d39cdde26dd51893c136e05ed36
Demon <demonsingur@gmail.com>
2017-10-18 19:04:07 +0000

    logicsquares: fix init cell referencing

--- logicsquares/js/Game.js
+++ logicsquares/js/Game.js
@@ -101,7 +101,7 @@
         gt.init = function() {
             for (var i = 0; i < size; i++) {
                 for (var j = 0; j < size; j++) {
-                    cell.initValue();
+                    cells[i][j].initValue();
                 }
             }
         };
